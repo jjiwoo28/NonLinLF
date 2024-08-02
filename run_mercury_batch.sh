fc597eff0778409082e83272701c765d88c84305
jjiwoo28 <jjiwoo28@gmail.com>
2024-08-02 10:48:06 +0000

    bash 추가

--- run_mercury_batch.sh
+++ run_mercury_batch.sh
@@ -1,9 +1,9 @@
-stanford_path="/data/NeuLF_rgb/stanford_half"
+stanford_path="/data/hmjung/NeuLF_rgb/dataset/stanford_half"
 # llff_path="/data/nerf_llff_data"
 # bmw_path="/data/bmw_dataset"
 
 
-test_day="240802_relu_skips2_d8w256_lr4e-4_exp_sc_batch8192"
+test_day="240802_relu_skips_d8w256_lr4e-4_exp_sc_batch8192"
 depth="8"
 width="256"
 epoch="300"
@@ -21,7 +21,7 @@ datasets=( "knights" "bracelet" "bunny" "tarot")
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset"
     
-    python wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip2 --lr 4e-4 --batch_size 8192
+    python wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip --lr 4e-4 --batch_size 8192
     
     python asem_json.py  "/data/result/${test_day}" "result_json/${test_day}"
 
@@ -29,7 +29,7 @@ done
 
 python asem_json.py  "/data/result/${test_day}" "result_json/${test_day}"
 
-test_day="240802_relu_skips2_d4w256_lr4e-4_exp_sc_batch8192"
+test_day="240802_relu_skips_d4w256_lr4e-4_exp_sc_batch8192"
 depth="4"
 width="256"
 epoch="300"
@@ -43,7 +43,7 @@ result_path="/data/result/${test_day}"
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset"
     
-    python  wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip2 --lr 4e-4 --batch_size 8192
+    python  wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip --lr 4e-4 --batch_size 8192
     
     python asem_json.py  "/data/result/${test_day}" "result_json/${test_day}"
 
@@ -52,7 +52,7 @@ done
 python asem_json.py  "/data/result/${test_day}" "result_json/${test_day}"
 
 
-test_day="240802_relu_skips2_d2w256_lr4e-4_exp_sc_batch8192"
+test_day="240802_relu_skips_d2w256_lr4e-4_exp_sc_batch8192"
 depth="2"
 width="256"
 epoch="300"
@@ -66,7 +66,7 @@ result_path="/data/result/${test_day}"
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset"
     
-    python  wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip2 --lr 4e-4 --batch_size 8192
+    python  wire_lf_new.py --data_dir "${stanford_path}/${dataset}" --exp_dir "${result_path}/${test_day}_${dataset}" --depth $depth --width $width --whole_epoch $epoch --test_freq 10 --nonlin relu_skip --lr 4e-4 --batch_size 8192
     
     python asem_json.py  "/data/result/${test_day}" "result_json/${test_day}"
 
